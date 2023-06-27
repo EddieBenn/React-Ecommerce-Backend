b1e96a33392a9b2d0b34bc9659380f383aa8aa04
EddieBenn <ndaobongedidiong56@gmail.com>
2023-06-27 07:26:38 +0100

    stripe route updated

--- src/routes/stripeRoute.ts
+++ src/routes/stripeRoute.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { handlePayment } from "../controllers/paymentController";
-const router = express.Router()
+
+const router = express.Router();
 
 
 router.post("/", handlePayment)
